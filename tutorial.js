b1d22bc933c863f693a45c96d5e74ca328393751
oyajikun <oyajikun>
2017-01-24 10:37:51 +0900

    Add tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,26 @@
+/**
+ * Created by tomoya.igarashi on 2017/01/23.
+ */
+
+if (window.console) {
+    console.log("tutorial.js : Welcome to JavaScript!");
+
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    var source = Rx.Observable.from(array);
+
+    var filtered = source.filter(function (x) {
+        if ((x % 3) == 0) {
+            return true;
+        }
+    });
+
+    var mapped = filtered.map(function (x) {
+        var xs = ["-", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+        return xs[x];
+    });
+
+    mapped.subscribe(function (x) {
+        console.log(x);
+    });
+}
