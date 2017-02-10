d12a2ae7ad2725eb4f283ffe2977682754ce3b92
oyajikun <oyajikun>
2017-02-10 11:48:25 +0900

    Implementation click stream

--- tutorial.js
+++ tutorial.js
@@ -5,17 +5,17 @@
 if (window.console) {
     console.log("tutorial.js : Welcome to JavaScript!");
 
-    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    var source = Rx.Observable.from(array);
+    let source = Rx.Observable.from(array);
 
-    var filtered = source.filter(function (x) {
+    let filtered = source.filter(function (x) {
         if ((x % 3) == 0) {
             return true;
         }
     });
 
-    var mapped = filtered.map(function (x) {
+    let mapped = filtered.map(function (x) {
         var xs = ["-", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
         return xs[x];
     });
@@ -24,30 +24,17 @@ if (window.console) {
         console.log(x);
     });
 
-    var two = Rx.Observable.timer(2000).mapTo("2secs");
-    var four = Rx.Observable.timer(4000).mapTo("4secs");
+    let two = Rx.Observable.timer(2000).mapTo("2secs");
+    let four = Rx.Observable.timer(4000).mapTo("4secs");
 
-    var merged = two.merge(four);
+    let merged = two.merge(four);
     merged.subscribe(function (x) {
         console.log(x);
     });
 
-    var observableA = Rx.Observable.interval(1000).take(5);
-    var observableB = Rx.Observable.interval(2000).take(5);
-    var observableC = Rx.Observable.interval(3000).take(5);
-
-    // 複雑な条件をpatternにします。
-    var pattern = observableA.and(observableB);
-
-    // thenでpatternが起きたらどうするかのplanを作ります。
-    var plan = pattern.thenDo(function (x, y, z) {
-        return "3つ揃ったよ!";
-    });
-
-    //whenでシーケンスを取り出そう。
-    var complexCondition = Rx.Observable.when(plan);
-
-    complexCondition.subscribe(function (x) {
-        console.log(x);
+    let button = document.getElementById("button");
+    let clickStream = Rx.Observable.fromEvent(button, 'click');
+    clickStream.subscribe(() => {
+        console.log("clicked");
     });
 }
