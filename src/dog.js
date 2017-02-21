6dc967e4e7ba47e85d8df8099d3edaf85995bdc2
oyajikun <oyajikun>
2017-02-21 12:33:13 +0900

    First gulp

---
+++ src/dog.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tomoya.igarashi on 2017/02/21.
+ */
+export default class {
+    constructor(name) {
+        this.name = name;
+    }
+
+    bark() {
+        return `Wah wah, I am ${this.name}`;
+    }
+}
